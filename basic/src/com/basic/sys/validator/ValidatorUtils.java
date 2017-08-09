f03e619ee414972763616bb751be407c24c135c2
bladedance <bladedance@vip.qq.com>
2017-08-09 10:40:34 +0800

    modify

--- basic/src/com/basic/sys/validator/ValidatorUtils.java
+++ basic/src/com/basic/sys/validator/ValidatorUtils.java
@@ -12,30 +12,62 @@ import java.util.Set;
  * hibernate-validator校验工具类
  *
  * 参考文档：http://docs.jboss.org/hibernate/validator/5.4/reference/en-US/html_single/
+ * 
+ * Hibernate validator:
+ * 
+ * Bean Validation 中内置的 constraint
+ * 
+ * @Null 被注释的元素必须为 null
+ * @NotNull 被注释的元素必须不为 null
+ * @AssertTrue 被注释的元素必须为 true
+ * @AssertFalse 被注释的元素必须为 false
+ * @Min(value) 被注释的元素必须是一个数字，其值必须大于等于指定的最小值
+ * @Max(value) 被注释的元素必须是一个数字，其值必须小于等于指定的最大值
+ * @DecimalMin(value) 被注释的元素必须是一个数字，其值必须大于等于指定的最小值
+ * @DecimalMax(value) 被注释的元素必须是一个数字，其值必须小于等于指定的最大值 @Size(max=, min=)
+ *                    被注释的元素的大小必须在指定的范围内
+ * @Digits (integer, fraction) 被注释的元素必须是一个数字，其值必须在可接受的范围内
+ * @Past 被注释的元素必须是一个过去的日期
+ * @Future 被注释的元素必须是一个将来的日期 @Pattern(regex=,flag=) 被注释的元素必须符合指定的正则表达式
+ * 
+ *         Hibernate Validator 附加的 constraint
+ * @NotBlank(message =) 验证字符串非null，且长度必须大于0
+ * @Email 被注释的元素必须是电子邮箱地址 @Length(min=,max=) 被注释的字符串的大小必须在指定的范围内
+ * @NotEmpty 被注释的字符串的必须非空 @Range(min=,max=,message=) 被注释的元素必须在合适的范围内
+ * 
+ * @NotNull 和 @NotEmpty 和@NotBlank
+ *          区别：http://stackoverflow.com/questions/17137307/in-hibernate-validator-4-1-what-is-the-difference-between-notnull-notempty
+ * @NotEmpty 用在集合类上面
+ * @NotBlank 用在String上面
+ * @NotNull 用在基本类型上
  *
  * @author bladedance
  * @email bladedance@vip.qq.com
  * @date 2017-06-15 10:50
  */
 public class ValidatorUtils {
-    private static Validator validator;
+	private static Validator validator;
 
-    static {
-        validator = Validation.buildDefaultValidatorFactory().getValidator();
-    }
+	static {
+		validator = Validation.buildDefaultValidatorFactory().getValidator();
+	}
 
-    /**
-     * 校验对象
-     * @param object        待校验对象
-     * @param groups        待校验的组
-     * @throws RRException  校验不通过，则报RRException异常
-     */
-    public static void validateEntity(Object object, Class<?>... groups)
-            throws RRException {
-        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(object, groups);
-        if (!constraintViolations.isEmpty()) {
-        	ConstraintViolation<Object> constraint = (ConstraintViolation<Object>)constraintViolations.iterator().next();
-            throw new RRException(constraint.getMessage());
-        }
-    }
+	/**
+	 * 校验对象
+	 * 
+	 * @param object
+	 *            待校验对象
+	 * @param groups
+	 *            待校验的组
+	 * @throws RRException
+	 *             校验不通过，则报RRException异常
+	 */
+	public static void validateEntity(Object object, Class<?>... groups) throws RRException {
+		Set<ConstraintViolation<Object>> constraintViolations = validator.validate(object, groups);
+		if (!constraintViolations.isEmpty()) {
+			ConstraintViolation<Object> constraint = (ConstraintViolation<Object>) constraintViolations.iterator()
+					.next();
+			throw new RRException(constraint.getMessage());
+		}
+	}
 }
