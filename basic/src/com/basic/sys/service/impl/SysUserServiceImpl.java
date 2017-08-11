d1c82cc032559f748ac429f7ee8002e2564c24b4
bladedance <bladedance@vip.qq.com>
2017-08-11 11:24:29 +0800

    delete userrole when delete user

--- basic/src/com/basic/sys/service/impl/SysUserServiceImpl.java
+++ basic/src/com/basic/sys/service/impl/SysUserServiceImpl.java
@@ -19,8 +19,6 @@ import com.basic.sys.service.SysUserService;
 import com.basic.sys.utils.Constant;
 import com.basic.sys.utils.RRException;
 
-
-
 /**
  * 系统用户
  * 
@@ -51,17 +49,17 @@ public class SysUserServiceImpl implements SysUserService {
 	public SysUserEntity queryByUserName(String username) {
 		return sysUserDao.queryByUserName(username);
 	}
-	
+
 	@Override
 	public SysUserEntity queryObject(Long userId) {
 		return sysUserDao.queryObject(userId);
 	}
 
 	@Override
-	public List<SysUserEntity> queryList(Map<String, Object> map){
+	public List<SysUserEntity> queryList(Map<String, Object> map) {
 		return sysUserDao.queryList(map);
 	}
-	
+
 	@Override
 	public int queryTotal(Map<String, Object> map) {
 		return sysUserDao.queryTotal(map);
@@ -71,37 +69,41 @@ public class SysUserServiceImpl implements SysUserService {
 	@Transactional
 	public void save(SysUserEntity user) {
 		user.setCreateTime(new Date());
-		//sha256加密
+		// sha256加密
 		user.setPassword(new Sha256Hash(user.getPassword()).toHex());
 		sysUserDao.save(user);
-		
-		//检查角色是否越权
+
+		// 检查角色是否越权
 		checkRole(user);
-		
-		//保存用户与角色关系
+
+		// 保存用户与角色关系
 		sysUserRoleService.saveOrUpdate(user.getUserId(), user.getRoleIdList());
 	}
 
 	@Override
 	@Transactional
 	public void update(SysUserEntity user) {
-		if(StringUtils.isBlank(user.getPassword())){
+		if (StringUtils.isBlank(user.getPassword())) {
 			user.setPassword(null);
-		}else{
+		} else {
 			user.setPassword(new Sha256Hash(user.getPassword()).toHex());
 		}
 		sysUserDao.update(user);
-		
-		//检查角色是否越权
+
+		// 检查角色是否越权
 		checkRole(user);
-		
-		//保存用户与角色关系
+
+		// 保存用户与角色关系
 		sysUserRoleService.saveOrUpdate(user.getUserId(), user.getRoleIdList());
 	}
 
 	@Override
 	@Transactional
 	public void deleteBatch(Long[] userId) {
+		// 删除用户角色关系
+		for (Long userid : userId) {
+			sysUserRoleService.delete(userid);
+		}
 		sysUserDao.deleteBatch(userId);
 	}
 
@@ -113,21 +115,21 @@ public class SysUserServiceImpl implements SysUserService {
 		map.put("newPassword", newPassword);
 		return sysUserDao.updatePassword(map);
 	}
-	
+
 	/**
 	 * 检查角色是否越权
 	 */
-	private void checkRole(SysUserEntity user){
-		//如果不是超级管理员，则需要判断用户的角色是否自己创建
-		if(user.getCreateUserId() == Constant.SUPER_ADMIN){
-			return ;
+	private void checkRole(SysUserEntity user) {
+		// 如果不是超级管理员，则需要判断用户的角色是否自己创建
+		if (user.getCreateUserId() == Constant.SUPER_ADMIN) {
+			return;
 		}
-		
-		//查询用户创建的角色列表
+
+		// 查询用户创建的角色列表
 		List<Long> roleIdList = sysRoleService.queryRoleIdList(user.getCreateUserId());
-		
-		//判断是否越权
-		if(!roleIdList.containsAll(user.getRoleIdList())){
+
+		// 判断是否越权
+		if (!roleIdList.containsAll(user.getRoleIdList())) {
 			throw new RRException("新增用户所选角色，不是本人创建");
 		}
 	}
